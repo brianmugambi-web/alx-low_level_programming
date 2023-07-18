8e5ad5e1faad8dc6bce9a7e3229b601ed03cab69
brianmugambi-web <brianmugambi370@gmail.com>
2023-07-17 23:45:19 -0700

    fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Returns the absolute value of an integer number.
+ * @c: The number to check.
+ *
+ * Return: The absolute value of @c.
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
